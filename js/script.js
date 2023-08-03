36ce9065cf9e139298487cbcc5d2924f4c348a17
Upek Hansaja <upekhansajabk@gmail.com>
2023-08-03 20:17:06 +0530

    update preloader

---
+++ js/script.js
@@ -0,0 +1,8 @@
+if (document.addEventListener(document.getElementById("container3D"), true)) {
+
+    var preLoader = document.getElementById("preLoader");
+
+    window.addEventListener("load", function () {
+        preLoader.style.display = "none";
+    });
+}
