513aec58575dbe72a0e445617a586ad0d03fc77d
Upek Hansaja <upekhansajabk@gmail.com>
2023-08-03 22:47:57 +0530

    update preloader

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
-if (document.addEventListener(document.getElementById("container3D"), true)) {
+var preLoader = document.getElementById("preLoader");
+var canvas = document.querySelectorAll("#container3D canvas");
 
-    var preLoader = document.getElementById("preLoader");
-
-    window.addEventListener("load", function () {
-        preLoader.style.display = "none";
-    });
+function checkImage(imageSrc, good, bad) {
+  var img = new Image();
+  img.onload = good;
+  img.onerror = bad;
+  img.src = imageSrc;
 }
+
+checkImage(
+  "models/halo/textures/MKV_Helmet_normal.png",
+  function () {
+    console.log("model image loaded");
+    window.addEventListener("load", function () {
+      preLoader.classList.remove = "loaderImg";
+      preLoader.classList.add = "fade-out";
+      if ((preLoader.classList.contains = "fade-out")) {
+        preLoader.style.display = "none";
+      }
+    });
+  },
+  function () {
+    console.log("wating for 3D model to load...");
+  }
+);
